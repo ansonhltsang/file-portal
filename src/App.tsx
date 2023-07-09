20002bf06ec7eda10fb6b7c0d29bc4e6d6640fdb
anson <ansonhltsang@gmail.com>
2023-07-09 22:51:08 +0100

    Scaffolding frontend

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+import 'bootswatch/dist/minty/bootstrap.min.css';
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <p>Hello World!</p>
+    </>
+  );
+}
