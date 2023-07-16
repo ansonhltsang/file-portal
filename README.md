b05da9827d59cd245ea4c6bd04d355653a58716f
anson <ansonhltsang@gmail.com>
2023-07-16 19:58:45 +0100

    Database schema with migration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # file-portal
+
+To run pocketbase locally: `go run main.go serve --http=127.0.0.1:8090`
--- migrations/1689464179_initial_migration.go
+++ migrations/1689464179_initial_migration.go
@@ -2,16 +2,108 @@ package migrations
 
 import (
 	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
 	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/models/schema"
+	"github.com/pocketbase/pocketbase/tools/types"
 )
 
 func init() {
-	m.Register(func(db dbx.Builder) error {
+    m.Register(func(db dbx.Builder) error {
+        dao := daos.New(db)
 
-		return nil
-	}, func(db dbx.Builder) error {
-		// add down queries...
+        // App settings
+        settings, _ := dao.FindSettings()
+        settings.Meta.AppName = "File Portal"
+        settings.Smtp.Enabled = false
+		settings.Logs.MaxDays = 7
+        if err := dao.SaveSettings(settings); err != nil {
+            return err
+        }
 
-		return nil
-	})
+		// Remove default Users collection
+		userCollection, err := dao.FindCollectionByNameOrId("users")
+		if err != nil {
+			return err
+		}
+
+		if err := dao.DeleteCollection(userCollection); err != nil {
+			return err
+		}
+
+		/*
+		API Rules
+		nil - action only performed by an authorised admin
+		empty string - anyone can perform the action
+		non-empty string - anyone that satisfy the rule filter can perform the action
+		*/
+
+		// Create Sessions collection
+		sessionsCollection := &models.Collection{
+			Name:       "sessions",
+			Type:       models.CollectionTypeBase,
+			ListRule:   nil,
+			ViewRule:   types.Pointer(""),
+			CreateRule: types.Pointer(""),
+			UpdateRule: nil,
+			DeleteRule: nil,
+			Schema:     schema.NewSchema(
+				&schema.SchemaField{
+					Name:     "expired",
+					Type:     schema.FieldTypeDate,
+					Required: true,
+				},
+			),
+		}
+
+		if err := dao.SaveCollection(sessionsCollection); err != nil {
+			return err
+		}
+
+		// Create Files collection
+		filesCollection := &models.Collection{
+			Name:       "files",
+			Type:       models.CollectionTypeBase,
+			ListRule:   nil,
+			ViewRule:   types.Pointer(""),
+			CreateRule: types.Pointer("@request.date.expired > @now"),
+			UpdateRule: types.Pointer("@request.date.expired > @now && @request.headers.session_id = session"),
+			DeleteRule: types.Pointer("@request.headers.session_id = session"),
+			Schema:     schema.NewSchema(
+				&schema.SchemaField{
+					Name:     "session",
+					Type:     schema.FieldTypeRelation,
+					Required: true,
+					Options:  &schema.RelationOptions{
+						MaxSelect: types.Pointer(1),
+						CollectionId: sessionsCollection.Id,
+					},
+				},
+				&schema.SchemaField{
+					Name:     "expired",
+					Type:     schema.FieldTypeDate,
+					Required: true,
+				},
+				&schema.SchemaField{
+					Name:     "file",
+					Type:     schema.FieldTypeFile,
+					Required: true,
+					Options: &schema.FileOptions{
+						MaxSize: 10485760, // 10 MB file size limit
+						MaxSelect: 1,
+					},
+				},
+			),
+		}
+
+		if err := dao.SaveCollection(filesCollection); err != nil {
+			return err
+		}
+
+        return nil
+    }, func(db dbx.Builder) error {
+		// Down migration not needed
+        return nil
+    })
 }
